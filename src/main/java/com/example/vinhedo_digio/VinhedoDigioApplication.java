1d564505c4e8e74f89fc05dce993b74b35d4ad3d
pedroalcantara9568 <alcantara_guitar@hotmail.com>
2024-08-05 11:27:47 -0300

    primeiro commit

---
+++ src/main/java/com/example/vinhedo_digio/VinhedoDigioApplication.java
@@ -0,0 +1,13 @@
+package com.example.vinhedo_digio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VinhedoDigioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VinhedoDigioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/vinhedo_digio/VinhedoDigioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vinhedo_digio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VinhedoDigioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
